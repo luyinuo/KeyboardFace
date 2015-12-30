30e40728181d1779b47f913ed04512f190f123a8
luyinuo <maiziedu@jialin.local>
2015-12-30 10:02:40 +0800

    Initial Commit

---
+++ KeyboardFace/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KeyboardFace
+//
+//  Created by Maiziedu on 15/12/30.
+//  Copyright (c) 2015年 com.lyn.TestTimer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ KeyboardFaceTests/KeyboardFaceTests.m
@@ -0,0 +1,40 @@
+//
+//  KeyboardFaceTests.m
+//  KeyboardFaceTests
+//
+//  Created by Maiziedu on 15/12/30.
+//  Copyright (c) 2015年 com.lyn.TestTimer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface KeyboardFaceTests : XCTestCase
+
+@end
+
+@implementation KeyboardFaceTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
